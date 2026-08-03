115cbc97df8a96643a52126761f9257fbf5cc650
Shrushti Shahapur <shrushtishahapur349@gmail.com>
2026-08-03 22:18:00 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,42 @@
+#arithmetic operator
+a=10
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+#relational operator
+a=20
+b=50
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+print(a>b)
+print(a<b)
+#assignment operator
+num=10
+num *= 5
+print("num:",num)
+#logical opearator
+a=50
+b=30
+print(not False)
+print(not True)
+print(not(a>b))
+
+val1=True
+val2=False
+print("AND operator:",val1 and val2)
+print("OR operator:",val1 or val2)
+
+#type conversion
+a=2
+b=4.25
+sum=a+b
+print(sum)
+
+name=input("enter your name:")
+print("welcome:",name)
+
