9a3911669836bfb5d0b41d50087bc60d4115dd3f
Shrushti Shahapur <shrushtishahapur349@gmail.com>
2026-08-03 22:06:42 +0530

    Add files via upload

---
+++ firstprg.py
@@ -0,0 +1,18 @@
+name="shrushti"
+age=20
+price=25.99
+print("my name is:",name)
+print("my age is :",age)
+print(type(name))
+print(type(age))
+print(type(price))
+name1='sony'
+name2='''sru'''
+print(name1)
+print(name2)
+age=19
+old=False
+a=None
+print(type(old))
+print(type(age))
+print(type(a))
